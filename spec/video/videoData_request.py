162eed12634a829bd631dee0e8e09378f89574be
dpwlsssss <yaezinny95@naver.com>
2025-09-23 16:25:39 +0900

    Update:videoData_*.py생성코드 구현

---
+++ spec/video/videoData_request.py
@@ -0,0 +1,268 @@
+# 자동 생성된 VideoRequest 파일
+# JSON 데이터를 기반으로 생성되었습니다. (request 모드)
+
+# Authentication
+Authentication_out_data = {
+    "code": "200",
+    "message": "성공",
+    "userName": "관리자",
+    "userAff": "오산시청",
+    "accessToken": "abcde1234"
+}
+
+# Capabilities
+Capabilities_out_data = {
+    "code": "200",
+    "message": "성공",
+    "streamingSupport": [
+        {
+        "streamProtocolType": "RTSP",
+        "streamProtocolDesc": "Unicast"
+    }
+],
+    "transportSupport": [
+        {
+        "transProtocolType": "REST_API",
+        "transProtocolDesc": "desc"
+    }
+]
+}
+
+# CameraProfiles
+CameraProfiles_out_data = {
+    "code": "200",
+    "message": "성공",
+    "camList": [
+        {
+        "camID": "cam0001",
+        "camName": "카메라1",
+        "camLoc": {
+        "lon": "127.127730",
+        "lat": "38.439801",
+        "alt": "32.131",
+        "desc": "3층복도"
+    },
+        "camConfig": {
+        "camType": "PTZ"
+    }
+    },
+        {
+        "camID": "cam0002",
+        "camName": "카메라2",
+        "camLoc": {
+        "lon": "127.2887",
+        "lat": "37.33671"
+    },
+        "camConfig": {
+        "camType": "dome"
+    }
+    }
+]
+}
+
+# StoredVideoInfos
+StoredVideoInfos_out_data = {
+    "code": "200",
+    "message": "성공",
+    "camList": [
+        {
+        "camID": "cam0001",
+        "timeList": [
+            {
+            "startTime": 20220822163022123,
+            "endTime": 20220822163025123
+        }
+    ]
+    },
+        {
+        "camID": "cam0002",
+        "timeList": [
+            {
+            "startTime": 20220822163022123,
+            "endTime": 20220822163025123
+        }
+    ]
+    }
+]
+}
+
+# StreamURLs
+StreamURLs_out_data = {
+    "code": "200",
+    "message": "성공",
+    "camList": [
+        {
+        "camID": "cam001",
+        "accessID": "conn001",
+        "accessPW": "1234",
+        "camURL": "rtsp://192.168.0.1:8000"
+    },
+        {
+        "camID": "cam002",
+        "accessID": "conn002",
+        "accessPW": "1234",
+        "camURL": "rtsp://192.168.0.2:8000"
+    },
+        {
+        "camID": "cam003",
+        "accessID": "conn003",
+        "accessPW": "1234",
+        "camURL": "rtsp://192.168.0.3:8000"
+    }
+]
+}
+
+# ReplayURL
+ReplayURL_out_data = {
+    "code": "200",
+    "message": "성공",
+    "camList": [
+        {
+        "camID": "cam0001",
+        "accessID": "conn0001",
+        "accessPW": "1234",
+        "startTime": 20220822163022123,
+        "endTime": 20220822163025123,
+        "camURL": "rtsp://192.168.0.1:8000"
+    },
+        {
+        "camID": "cam0002",
+        "accessID": "conn0002",
+        "accessPW": "1234",
+        "startTime": 20220822163022123,
+        "endTime": 20220822163025123,
+        "camURL": "rtsp://192.168.0.2:8000"
+    },
+        {
+        "camID": "cam0003",
+        "accessID": "conn0003",
+        "accessPW": "1234",
+        "startTime": 20220822163022123,
+        "endTime": 20220822163025123,
+        "camURL": "rtsp://192.168.0.3:8000"
+    }
+]
+}
+
+# RealtimeVideoEventInfos
+RealtimeVideoEventInfos_out_data = {
+    "camList": [
+        {
+        "camID": "cam0001",
+        "eventUUID": "event01",
+        "eventName": "배회",
+        "startTime": 20220822163022123,
+        "endTime": 20220822163025123,
+        "eventDesc": "sfdfEFASDDDLKJFjdkdlfjde"
+    },
+        {
+        "camID": "cam0002",
+        "eventUUID": "event01",
+        "eventName": "배회",
+        "startTime": 20220822163022123,
+        "endTime": 20220822163025123,
+        "eventDesc": "dddddeeeeeAAFEDFiikjf"
+    }
+]
+}
+
+# StoredVideoEventInfos
+StoredVideoEventInfos_out_data = {
+    "code": "200",
+    "message": "성공",
+    "camList": [
+        {
+        "camID": "cam0001",
+        "eventUUID": "event01",
+        "eventName": "배회",
+        "startTime": 20220822163022123,
+        "endTime": 20220822163025123,
+        "eventDesc": "AAABVVVVCCCDDssvfdd"
+    },
+        {
+        "camID": "cam0002",
+        "eventUUID": "event01",
+        "eventName": "배회",
+        "startTime": 20220822163022123,
+        "endTime": 20220822163025123,
+        "eventDesc": "FFFeeiiiWWkdjflskdjfoEKK"
+    },
+        {
+        "camID": "cam0003",
+        "eventUUID": "event01",
+        "eventName": "배회",
+        "startTime": 20220822163022123,
+        "endTime": 20220822163025123,
+        "eventDesc": "iVUhEUgAAACAAAAAgCAYAAA"
+    }
+]
+}
+
+# StoredObjectAnalyticsInfos
+StoredObjectAnalyticsInfos_out_data = {
+    "code": "200",
+    "message": "성공",
+    "camList": [
+        {
+        "camID": "cam0001",
+        "analyticsTime": 20220822163022123,
+        "anlayticsResultList": [
+            {
+            "anayticsID": "object001",
+            "analyticsClass": "차량",
+            "analyticsConfidence": 0.8,
+            "analyticsBoundingBox": {
+            "left": 0.2,
+            "top": 0.1,
+            "right": 0.5,
+            "bottom": 0.6
+        },
+            "analyticsDesc": "13가 4567"
+        }
+    ]
+    },
+        {
+        "camID": "cam0002",
+        "analyticsTime": 20220822163022123,
+        "anlayticsResultList": [
+            {
+            "anayticsID": "object002",
+            "analyticsClass": "사람",
+            "analyticsConfidence": 0.9,
+            "analyticsBoundingBox": {
+            "left": 0.3,
+            "top": 0.1,
+            "right": 0.5,
+            "bottom": 0.7
+        }
+        }
+    ]
+    }
+]
+}
+
+# steps 순서대로 출력 메시지 리스트 생성
+videoOutMessage = [
+    Authentication_out_data,
+    Capabilities_out_data,
+    CameraProfiles_out_data,
+    StoredVideoInfos_out_data,
+    StreamURLs_out_data,
+    ReplayURL_out_data,
+    RealtimeVideoEventInfos_out_data,
+    StoredVideoEventInfos_out_data,
+    StoredObjectAnalyticsInfos_out_data,
+]
+
+# API endpoint 이름 리스트
+videoMessages = [
+    "Authentication",
+    "Capabilities",
+    "CameraProfiles",
+    "StoredVideoInfos",
+    "StreamURLs",
+    "ReplayURL",
+    "RealtimeVideoEventInfos",
+    "StoredVideoEventInfos",
+    "StoredObjectAnalyticsInfos",
+]
---
+++ spec/video/videoData_response.py
@@ -0,0 +1,164 @@
+# 자동 생성된 VideoRequest 파일
+# JSON 데이터를 기반으로 생성되었습니다. (response 모드)
+
+# Authentication
+Authentication_in_data = {
+    "userID": "user001",
+    "userPW": "pass001"
+}
+
+# Capabilities
+Capabilities_in_data = {}
+
+# CameraProfiles
+CameraProfiles_in_data = {}
+
+# StoredVideoInfos
+StoredVideoInfos_in_data = {
+    "timePeriod": {
+    "startTime": 20220822163022123,
+    "endTime": 20220822163025123
+},
+    "camList": [
+        {
+        "camID": "cam001"
+    },
+        {
+        "camID": "cam002"
+    }
+]
+}
+
+# StreamURLs
+StreamURLs_in_data = {
+    "camList": [
+        {
+        "camID": "cam001",
+        "streamProtocolType": "RTSP"
+    }
+]
+}
+
+# ReplayURL
+ReplayURL_in_data = {
+    "camList": [
+        {
+        "camID": "cam001",
+        "startTime": 20220822163022123,
+        "endTime": 20220822163025123,
+        "streamProtocolType": "RTSP"
+    },
+        {
+        "camID": "cam002",
+        "startTime": 20220822163022123,
+        "endTime": 20220822163025123,
+        "streamProtocolType": "RTSP"
+    },
+        {
+        "camID": "cam003",
+        "startTime": 20220822163022123,
+        "endTime": 20220822163025123,
+        "streamProtocolType": "RTSP"
+    }
+]
+}
+
+# RealtimeVideoEventInfos
+RealtimeVideoEventInfos_in_data = {
+    "camList": [
+        {
+        "camID": "cam001"
+    },
+        {
+        "camID": "cam002"
+    }
+],
+    "transProtocol": {
+    "transProtocolType": "LongPolling"
+},
+    "duration": 10,
+    "eventFilter": "배회",
+    "startTime": 20220822163022123
+}
+
+# StoredVideoEventInfos
+StoredVideoEventInfos_in_data = {
+    "code": "200",
+    "message": "성공",
+    "camList": [
+        {
+        "camID": "cam0001",
+        "eventUUID": "event01",
+        "eventName": "배회",
+        "startTime": 20220822163022123,
+        "endTime": 20220822163025123,
+        "eventDesc": "AAABVVVVCCCDDssvfdd"
+    },
+        {
+        "camID": "cam0002",
+        "eventUUID": "event01",
+        "eventName": "배회",
+        "startTime": 20220822163022123,
+        "endTime": 20220822163025123,
+        "eventDesc": "FFFeeiiiWWkdjflskdjfoEKK"
+    },
+        {
+        "camID": "cam0003",
+        "eventUUID": "event01",
+        "eventName": "배회",
+        "startTime": 20220822163022123,
+        "endTime": 20220822163025123,
+        "eventDesc": "iVUhEUgAAACAAAAAgCAYAAA"
+    }
+]
+}
+
+# StoredObjectAnalyticsInfos
+StoredObjectAnalyticsInfos_in_data = {
+    "timePeriod": {
+    "startTime": 20220822163022123,
+    "endTime": 20220822163025123
+},
+    "camList": [
+        {
+        "camID": "cam0001"
+    },
+        {
+        "camID": "cam0002"
+    },
+        {
+        "camID": "cam0003"
+    }
+],
+    "anayticsFilter": "객체탐지",
+    "classFilter": [
+    "차량",
+    "사람"
+]
+}
+
+# steps 순서대로 입력 메시지 리스트 생성
+videoInMessage = [
+    Authentication_in_data,
+    Capabilities_in_data,
+    CameraProfiles_in_data,
+    StoredVideoInfos_in_data,
+    StreamURLs_in_data,
+    ReplayURL_in_data,
+    RealtimeVideoEventInfos_in_data,
+    StoredVideoEventInfos_in_data,
+    StoredObjectAnalyticsInfos_in_data,
+]
+
+# API endpoint 이름 리스트
+videoMessages = [
+    "Authentication",
+    "Capabilities",
+    "CameraProfiles",
+    "StoredVideoInfos",
+    "StreamURLs",
+    "ReplayURL",
+    "RealtimeVideoEventInfos",
+    "StoredVideoEventInfos",
+    "StoredObjectAnalyticsInfos",
+]
