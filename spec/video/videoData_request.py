5376830a20616b3a93bbfeeec7086c37f0916115
dpwlsssss <yaezinny95@naver.com>
2025-09-25 15:41:45 +0900

    Update: WebHook모드schema/video 생성, CONSTANTS파일 덮어씌우기 구현

--- spec/video/videoData_request.py
+++ spec/video/videoData_request.py
@@ -1,5 +1,5 @@
 # 자동 생성된 VideoRequest 파일
-# JSON 데이터를 기반으로 생성되었습니다. (request 모드)
+# request 모드
 
 # Authentication
 Authentication_out_data = {
@@ -23,7 +23,7 @@ Capabilities_out_data = {
     "transportSupport": [
         {
         "transProtocolType": "REST_API",
-        "transProtocolDesc": "desc"
+        "transProtocolDesc": "trans protocol description"
     }
 ]
 }
@@ -87,30 +87,7 @@ StoredVideoInfos_out_data = {
 }
 
 # StreamURLs
-StreamURLs_out_data = {
-    "code": "200",
-    "message": "성공",
-    "camList": [
-        {
-        "camID": "cam001",
-        "accessID": "conn001",
-        "accessPW": "1234",
-        "camURL": "rtsp://192.168.0.1:8000"
-    },
-        {
-        "camID": "cam002",
-        "accessID": "conn002",
-        "accessPW": "1234",
-        "camURL": "rtsp://192.168.0.2:8000"
-    },
-        {
-        "camID": "cam003",
-        "accessID": "conn003",
-        "accessPW": "1234",
-        "camURL": "rtsp://192.168.0.3:8000"
-    }
-]
-}
+StreamURLs_out_data = {}
 
 # ReplayURL
 ReplayURL_out_data = {
@@ -146,24 +123,8 @@ ReplayURL_out_data = {
 
 # RealtimeVideoEventInfos
 RealtimeVideoEventInfos_out_data = {
-    "camList": [
-        {
-        "camID": "cam0001",
-        "eventUUID": "event01",
-        "eventName": "배회",
-        "startTime": 20220822163022123,
-        "endTime": 20220822163025123,
-        "eventDesc": "sfdfEFASDDDLKJFjdkdlfjde"
-    },
-        {
-        "camID": "cam0002",
-        "eventUUID": "event01",
-        "eventName": "배회",
-        "startTime": 20220822163022123,
-        "endTime": 20220822163025123,
-        "eventDesc": "dddddeeeeeAAFEDFiikjf"
-    }
-]
+    "code": 200,
+    "message": "성공"
 }
 
 # StoredVideoEventInfos
@@ -241,7 +202,29 @@ StoredObjectAnalyticsInfos_out_data = {
 ]
 }
 
-# steps 순서대로 출력 메시지 리스트 생성
+# WebHook RealtimeVideoEventInfos
+RealtimeVideoEventInfos_in_data = {
+    "camList": [
+        {
+        "camID": "cam0001",
+        "eventUUID": "event01",
+        "eventName": "배회",
+        "startTime": 20220822163022123,
+        "endTime": 20220822163025123,
+        "eventDesc": "sfdfEFASDDDLKJFjdkdlfjde"
+    },
+        {
+        "camID": "cam0002",
+        "eventUUID": "event01",
+        "eventName": "배회",
+        "startTime": 20220822163022123,
+        "endTime": 20220822163025123,
+        "eventDesc": "dddddeeeeeAAFEDFiikjf"
+    }
+]
+}
+
+# steps 순서대로 출력 메시지 생성
 videoOutMessage = [
     Authentication_out_data,
     Capabilities_out_data,
@@ -254,7 +237,7 @@ videoOutMessage = [
     StoredObjectAnalyticsInfos_out_data,
 ]
 
-# API endpoint 이름 리스트
+# API endpoint
 videoMessages = [
     "Authentication",
     "Capabilities",
@@ -265,4 +248,9 @@ videoMessages = [
     "RealtimeVideoEventInfos",
     "StoredVideoEventInfos",
     "StoredObjectAnalyticsInfos",
+]
+
+# WebHook
+videoWebhookData = [
+    RealtimeVideoEventInfos_in_data,
 ]
