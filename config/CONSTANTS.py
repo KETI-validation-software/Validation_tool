f1afa8a5c168d4acd201ab974a66b1db27520b14
SooinJung <shanaren@sookmyung.ac.kr>
2025-10-30 11:23:08 +0900

    fix: 웹훅 플랫폼 응답 에러 수정

--- config/CONSTANTS.py
+++ config/CONSTANTS.py
@@ -22,7 +22,7 @@ url = "https://127.0.0.1:8080"
 contact_person = "김철수"
 model_name = "v1.0"
 request_id = "cmh1yg49v000t9y0jdr1wwuja"
-specs = [["cmgyv3rzl014nvsveidu5jpzp_outSchema","cmgyv3rzl014nvsveidu5jpzp_inData","cmgyv3rzl014nvsveidu5jpzp_messages",""]]
+specs = [["cmgvieyak001b6cd04cgaawmm_inSchema","cmgvieyak001b6cd04cgaawmm_outData","cmgvieyak001b6cd04cgaawmm_messages",""]]
 
 
 # opt 검증 - False 이면 검증 안함, 현재는 루프문에 의해 True인 상황 
@@ -56,16 +56,16 @@ enable_retry_delay = False  # False 권장: 불필요한 sleep 제거
 
 SPEC_CONFIG = [
     {
-        "group_name": "물리보안",
-        "group_id": "cmgzwohuq01y8vsvep0hpuuf4",
-        "cmgyv3rzl014nvsveidu5jpzp": {
+        "group_name": "통합플랫폼",
+        "group_id": "cmgzwocpp01y6vsve880sfysm",
+        "cmgvieyak001b6cd04cgaawmm": {
     "test_name": "영상보안시스템_new",
-    "specs": ['cmgyv3rzl014nvsveidu5jpzp_outSchema', 'cmgyv3rzl014nvsveidu5jpzp_inData', 'cmgyv3rzl014nvsveidu5jpzp_messages', 'cmgyv3rzl014nvsveidu5jpzp_webhook_inSchema', 'cmgyv3rzl014nvsveidu5jpzp_webhook_outData'],
+    "specs": ['cmgvieyak001b6cd04cgaawmm_inSchema', 'cmgvieyak001b6cd04cgaawmm_outData', 'cmgvieyak001b6cd04cgaawmm_messages', 'cmgvieyak001b6cd04cgaawmm_webhook_OutSchema', 'cmgvieyak001b6cd04cgaawmm_webhook_inData'],
     "api_name": ['인증', '전송 지원 기능 정보 연동', '카메라 목록 연동', '저장된 영상 목록 연동', '실시간 영상 정보 연동', '저장된 영상 정보 연동', '실시간 이벤트 분석 정보 연동', '저장된 이벤트 분석 정보 연동'],
-    "api_id": ['step-1-s9942zltt', 'step-1-vps6mosa3', 'step-3-dpd47yeve', 'step-4-nbpd1rfle', 'step-5-6juqjc7zt', 'step-6-2hxrdvfry', 'step-7-jgj9l9vf4', 'step-8-57zrt6i4l'],
+    "api_id": ['step-1-77lxvxrit', 'step-2-ln57b7ahz', 'step-1-354py0pz2', 'step-2-hyto4xrf1', 'step-5-dfxmyzzsw', 'step-6-pflcyt2cp', 'step-7-166n5ivbf', 'step-8-fff2u2432'],
     "api_endpoint": ['/Authentication', '/Capabilities', '/CameraProfiles', '/StoredVideoInfos', '/StreamURLs', '/ReplayURL', '/RealtimeVideoEventInfos', '/StoredVideoEventInfos'],
     "trans_protocol": ['basic', 'basic', 'basic', 'basic', 'basic', 'basic', 'WebHook', 'basic'],
-    "time_out": [5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000],
+    "time_out": [10000, 10000, 10000, 5000, 5000, 5000, 5000, 5000],
     "num_retries": [1, 1, 1, 1, 1, 1, 1, 1]
 }
     }
