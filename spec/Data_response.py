e22515dd32249489642f3fcb71b5d6f00720f3b4
dpwlsssss <yaezinny95@naver.com>
2025-11-03 09:57:17 +0900

    Revert "Merge pull request #191 from KETI-validation-software/info"
    This reverts commit d6633a1d77dfc80b4ad949e7303e697281eb56a2, reversing
    changes made to a368a594e1cb068a32c8c383d93fca90cb8ab1e6.

--- spec/Data_response.py
+++ spec/Data_response.py
@@ -1,127 +1,12 @@
 # response 모드
 
 # Authentication
-cmgyv3rzl014nvsveidu5jpzp_Authentication_in_data = {
-    "userID": "user0001",
-    "userPW": "pass0001"
-}
-
-# Capabilities
-cmgyv3rzl014nvsveidu5jpzp_Capabilities_in_data = {}
-
-# CameraProfiles
-cmgyv3rzl014nvsveidu5jpzp_CameraProfiles_in_data = {}
-
-# StoredVideoInfos
-cmgyv3rzl014nvsveidu5jpzp_StoredVideoInfos_in_data = {
-    "timePeriod": {
-    "startTime": 1760948700000,
-    "endTime": 1761121500000
-},
-    "camList": [
-        {
-        "camID": ""
-    }
-]
-}
-
-# StreamURLs
-cmgyv3rzl014nvsveidu5jpzp_StreamURLs_in_data = {
-    "camList": [
-        {
-        "camID": "",
-        "streamProtocolType": "RTSP"
-    }
-]
-}
-
-# ReplayURL
-cmgyv3rzl014nvsveidu5jpzp_ReplayURL_in_data = {
-    "camList": [
-        {
-        "camID": "",
-        "startTime": 1760949060000,
-        "endTime": 1761208260000,
-        "streamProtocolType": "RTSP"
-    }
-]
-}
-
-# RealtimeVideoEventInfos
-cmgyv3rzl014nvsveidu5jpzp_RealtimeVideoEventInfos_in_data = {
-    "camList": [
-        {
-        "camID": ""
-    }
-],
-    "duration": "100",
-    "transProtocol": {
-    "transProtocolType": "WebHook",
-    "transProtocolDesc": "127.0.0.1:8090"
-},
-    "eventFilter": "",
-    "classFilter": "",
-    "startTime": 1761110160000
-}
-
-# RealtimeVideoEventInfos WebHook OUT Data
-cmgyv3rzl014nvsveidu5jpzp_RealtimeVideoEventInfos_webhook_out_data = {
-    "code": "",
-    "message": ""
-}
-
-# StoredVideoEventInfos
-cmgyv3rzl014nvsveidu5jpzp_StoredVideoEventInfos_in_data = {
-    "timePeriod": {
-    "startTime": 1759221900000,
-    "endTime": 1761727500000
-},
-    "camList": [
-        {
-        "camID": ""
-    }
-],
-    "maxCount": 3,
-    "eventFilter": "",
-    "classFilter": ""
-}
-
-# cmgyv3rzl014nvsveidu5jpzp 데이터 리스트
-cmgyv3rzl014nvsveidu5jpzp_inData = [
-    cmgyv3rzl014nvsveidu5jpzp_Authentication_in_data,
-    cmgyv3rzl014nvsveidu5jpzp_Capabilities_in_data,
-    cmgyv3rzl014nvsveidu5jpzp_CameraProfiles_in_data,
-    cmgyv3rzl014nvsveidu5jpzp_StoredVideoInfos_in_data,
-    cmgyv3rzl014nvsveidu5jpzp_StreamURLs_in_data,
-    cmgyv3rzl014nvsveidu5jpzp_ReplayURL_in_data,
-    cmgyv3rzl014nvsveidu5jpzp_RealtimeVideoEventInfos_in_data,
-    cmgyv3rzl014nvsveidu5jpzp_StoredVideoEventInfos_in_data,
-]
-
-# cmgyv3rzl014nvsveidu5jpzp WebHook 데이터 리스트
-cmgyv3rzl014nvsveidu5jpzp_webhook_outData = [
-    cmgyv3rzl014nvsveidu5jpzp_RealtimeVideoEventInfos_webhook_out_data,
-]
-
-# cmgyv3rzl014nvsveidu5jpzp API endpoint
-cmgyv3rzl014nvsveidu5jpzp_messages = [
-    "Authentication",
-    "Capabilities",
-    "CameraProfiles",
-    "StoredVideoInfos",
-    "StreamURLs",
-    "ReplayURL",
-    "RealtimeVideoEventInfos",
-    "StoredVideoEventInfos",
-]
-
-# Authentication
 cmgvieyak001b6cd04cgaawmm_Authentication_out_data = {
     "code": "200",
     "message": "성공",
     "userName": "관리자",
     "userAff": "오산시청",
-    "accessToken": "abcde1234"
+    "accessToken": "a"
 }
 
 # Capabilities
