9fe1bcc34321624456e06ea88f2aee803aade4f0
SooinJung <shanaren@sookmyung.ac.kr>
2025-12-05 16:03:19 +0900

    update: 잘못된 요청에 대한 400 응답 에러 기능 추가

--- spec/Schema_request.py
+++ spec/Schema_request.py
@@ -2,7 +2,10 @@ from json_checker import OptionalKey
 
 
 # Authentication
-cmii7shen005i8z1tagevx4qh_Authentication_in_schema = {}
+cmii7shen005i8z1tagevx4qh_Authentication_in_schema = {
+    "userID": str,
+    "userPW": str,
+}
 
 # cmii7shen005i8z1tagevx4qh 스키마 리스트
 cmii7shen005i8z1tagevx4qh_inSchema = [
@@ -10,7 +13,10 @@ cmii7shen005i8z1tagevx4qh_inSchema = [
 ]
 
 # Authentication
-cmii7pysb004k8z1tts0npxfm_Authentication_in_schema = {}
+cmii7pysb004k8z1tts0npxfm_Authentication_in_schema = {
+    "userID": str,
+    "userPW": str,
+}
 
 # Capabilities
 cmii7pysb004k8z1tts0npxfm_Capabilities_in_schema = {}
@@ -22,10 +28,56 @@ cmii7pysb004k8z1tts0npxfm_inSchema = [
 ]
 
 # Authentication
-cmii7lxbn002s8z1t1i9uudf0_Authentication_in_schema = {}
+cmii7lxbn002s8z1t1i9uudf0_Authentication_in_schema = {
+    "userID": str,
+    "userPW": str,
+}
+
+# Capabilities
+cmii7lxbn002s8z1t1i9uudf0_Capabilities_in_schema = {}
+
+# CameraProfiles
+cmii7lxbn002s8z1t1i9uudf0_CameraProfiles_in_schema = {}
+
+# StreamURLs
+cmii7lxbn002s8z1t1i9uudf0_StreamURLs_in_schema = {}
+
+# RealtimeVideoEventInfos
+cmii7lxbn002s8z1t1i9uudf0_RealtimeVideoEventInfos_in_schema = {}
+
+# RealtimeVideoEventInfos WebHook OUT Schema
+cmii7lxbn002s8z1t1i9uudf0_RealtimeVideoEventInfos_webhook_out_schema = {
+    "code": str,
+    "message": str,
+}
+
+# StoredVideoInfos
+cmii7lxbn002s8z1t1i9uudf0_StoredVideoInfos_in_schema = {}
+
+# ReplayURL
+cmii7lxbn002s8z1t1i9uudf0_ReplayURL_in_schema = {}
+
+# StoredVideoEventInfos
+cmii7lxbn002s8z1t1i9uudf0_StoredVideoEventInfos_in_schema = {}
+
+# StoredObjectAnalyticsInfos
+cmii7lxbn002s8z1t1i9uudf0_StoredObjectAnalyticsInfos_in_schema = {}
 
 # cmii7lxbn002s8z1t1i9uudf0 스키마 리스트
 cmii7lxbn002s8z1t1i9uudf0_inSchema = [
     cmii7lxbn002s8z1t1i9uudf0_Authentication_in_schema,
+    cmii7lxbn002s8z1t1i9uudf0_Capabilities_in_schema,
+    cmii7lxbn002s8z1t1i9uudf0_CameraProfiles_in_schema,
+    cmii7lxbn002s8z1t1i9uudf0_StreamURLs_in_schema,
+    cmii7lxbn002s8z1t1i9uudf0_RealtimeVideoEventInfos_in_schema,
+    cmii7lxbn002s8z1t1i9uudf0_StoredVideoInfos_in_schema,
+    cmii7lxbn002s8z1t1i9uudf0_ReplayURL_in_schema,
+    cmii7lxbn002s8z1t1i9uudf0_StoredVideoEventInfos_in_schema,
+    cmii7lxbn002s8z1t1i9uudf0_StoredObjectAnalyticsInfos_in_schema,
+]
+
+# cmii7lxbn002s8z1t1i9uudf0 WebHook 스키마 리스트
+cmii7lxbn002s8z1t1i9uudf0_webhook_OutSchema = [
+    cmii7lxbn002s8z1t1i9uudf0_RealtimeVideoEventInfos_webhook_out_schema,
 ]
 
--- spec/Validation_response.py
+++ spec/Validation_response.py
@@ -1,5 +1,22 @@
 # Authentication
-cmii7wfuf006i8z1tcds6q69g_Authentication_out_validation = {}
+cmii7wfuf006i8z1tcds6q69g_Authentication_out_validation = {
+  "code": {
+    "enabled": True,
+    "validationType": "specified-value-match",
+    "allowedValues": [
+      "400"
+    ],
+    "score": 0
+  },
+  "message": {
+    "enabled": True,
+    "validationType": "specified-value-match",
+    "allowedValues": [
+      "잘못된 요청"
+    ],
+    "score": 0
+  }
+}
 
 # cmii7wfuf006i8z1tcds6q69g 검증 리스트
 cmii7wfuf006i8z1tcds6q69g_outValidation = [
@@ -7,7 +24,24 @@ cmii7wfuf006i8z1tcds6q69g_outValidation = [
 ]
 
 # Authentication
-cmii7w683006h8z1t7usnin5g_Authentication_out_validation = {}
+cmii7w683006h8z1t7usnin5g_Authentication_out_validation = {
+  "code": {
+    "enabled": True,
+    "validationType": "specified-value-match",
+    "allowedValues": [
+      "400"
+    ],
+    "score": 0
+  },
+  "message": {
+    "enabled": True,
+    "validationType": "specified-value-match",
+    "allowedValues": [
+      "잘못된 요청"
+    ],
+    "score": 0
+  }
+}
 
 # cmii7w683006h8z1t7usnin5g 검증 리스트
 cmii7w683006h8z1t7usnin5g_outValidation = [
@@ -15,10 +49,43 @@ cmii7w683006h8z1t7usnin5g_outValidation = [
 ]
 
 # Authentication
-cmii7v8pr006g8z1tvo55a50u_Authentication_out_validation = {}
+cmii7v8pr006g8z1tvo55a50u_Authentication_out_validation = {
+  "code": {
+    "enabled": True,
+    "validationType": "specified-value-match",
+    "allowedValues": [
+      "400"
+    ],
+    "score": 0
+  },
+  "message": {
+    "enabled": True,
+    "validationType": "specified-value-match",
+    "allowedValues": [
+      "잘못된 요청"
+    ],
+    "score": 0
+  }
+}
+
+# Capabilities
+cmii7v8pr006g8z1tvo55a50u_Capabilities_out_validation = {}
+
+# CameraProfiles
+cmii7v8pr006g8z1tvo55a50u_CameraProfiles_out_validation = {}
+
+# StreamURLs
+cmii7v8pr006g8z1tvo55a50u_StreamURLs_out_validation = {}
+
+# RealtimeVideoEventInfos
+cmii7v8pr006g8z1tvo55a50u_RealtimeVideoEventInfos_out_validation = {}
 
 # cmii7v8pr006g8z1tvo55a50u 검증 리스트
 cmii7v8pr006g8z1tvo55a50u_outValidation = [
     cmii7v8pr006g8z1tvo55a50u_Authentication_out_validation,
+    cmii7v8pr006g8z1tvo55a50u_Capabilities_out_validation,
+    cmii7v8pr006g8z1tvo55a50u_CameraProfiles_out_validation,
+    cmii7v8pr006g8z1tvo55a50u_StreamURLs_out_validation,
+    cmii7v8pr006g8z1tvo55a50u_RealtimeVideoEventInfos_out_validation,
 ]
 
