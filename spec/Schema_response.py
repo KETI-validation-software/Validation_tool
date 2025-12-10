6e15bc05b11f48880e98f4ae3b1f911b69247398
SooinJung <shanaren@sookmyung.ac.kr>
2025-12-10 14:31:11 +0900

    fix: 스키마 생성 로직 수정

--- spec/Schema_response.py
+++ spec/Schema_response.py
@@ -2,35 +2,7 @@ from json_checker import OptionalKey
 
 
 # Authentication
-cmii7wfuf006i8z1tcds6q69g_Authentication_out_schema = {
-    "code": str,
-    "message": str,
-    "userName": str,
-    "userAff": str,
-    OptionalKey("accessToken"): str,
-}
-
-# cmii7wfuf006i8z1tcds6q69g 스키마 리스트
-cmii7wfuf006i8z1tcds6q69g_outSchema = [
-    cmii7wfuf006i8z1tcds6q69g_Authentication_out_schema,
-]
-
-# Authentication
-cmii7w683006h8z1t7usnin5g_Authentication_out_schema = {
-    "code": str,
-    "message": str,
-    "userName": str,
-    "userAff": str,
-    OptionalKey("accessToken"): str,
-}
-
-# cmii7w683006h8z1t7usnin5g 스키마 리스트
-cmii7w683006h8z1t7usnin5g_outSchema = [
-    cmii7w683006h8z1t7usnin5g_Authentication_out_schema,
-]
-
-# Authentication
-cmii7v8pr006g8z1tvo55a50u_Authentication_out_schema = {
+cmiqr2b9j00i9ie8frw439h8i_Authentication_out_schema = {
     "code": str,
     "message": str,
     "userName": str,
@@ -39,46 +11,238 @@ cmii7v8pr006g8z1tvo55a50u_Authentication_out_schema = {
 }
 
 # Capabilities
-cmii7v8pr006g8z1tvo55a50u_Capabilities_out_schema = {
+cmiqr2b9j00i9ie8frw439h8i_Capabilities_out_schema = {
     "code": str,
     "message": str,
-    "streamingSupport": [str],
-    "transportSupport": [str],
+    "transportSupport": [{
+    "transProtocolType": str,
+    OptionalKey("transProtocolDesc"): str,
+}],
+}
+
+# SensorDeviceProfiles
+cmiqr2b9j00i9ie8frw439h8i_SensorDeviceProfiles_out_schema = {
+    "code": str,
+    "message": str,
+    "sensorDeviceList": [{
+    "sensorDeviceID": str,
+    "sensorDeviceType": str,
+    "sensorDeviceName": str,
+    OptionalKey("sensorDeviceLoc"): {
+    "lon": str,
+    "lat": str,
+    OptionalKey("alt"): str,
+    OptionalKey("desc"): str,
+},
+}],
+}
+
+# SensorDeviceControl
+cmiqr2b9j00i9ie8frw439h8i_SensorDeviceControl_out_schema = {
+    "code": str,
+    "message": str,
+    "sensorDeviceID": str,
+    "sensorDeviceStatus": str,
+}
+
+# SensorDeviceControl2
+cmiqr2b9j00i9ie8frw439h8i_SensorDeviceControl2_out_schema = {
+    "code": str,
+    "message": str,
+    "sensorDeviceID": str,
+    "sensorDeviceStatus": str,
+}
+
+# cmiqr2b9j00i9ie8frw439h8i 스키마 리스트
+cmiqr2b9j00i9ie8frw439h8i_outSchema = [
+    cmiqr2b9j00i9ie8frw439h8i_Authentication_out_schema,
+    cmiqr2b9j00i9ie8frw439h8i_Capabilities_out_schema,
+    cmiqr2b9j00i9ie8frw439h8i_SensorDeviceProfiles_out_schema,
+    cmiqr2b9j00i9ie8frw439h8i_SensorDeviceControl_out_schema,
+    cmiqr2b9j00i9ie8frw439h8i_SensorDeviceControl2_out_schema,
+]
+
+# Authentication
+cmiqr1jha00i6ie8fb1scb3go_Authentication_out_schema = {
+    "code": str,
+    "message": str,
+    "userName": str,
+    "userAff": str,
+    OptionalKey("accessToken"): str,
+}
+
+# Capabilities
+cmiqr1jha00i6ie8fb1scb3go_Capabilities_out_schema = {
+    "code": str,
+    "message": str,
+    "transportSupport": [{
+    "transProtocolType": str,
+    OptionalKey("transProtocolDesc"): str,
+}],
+}
+
+# DoorProfiles
+cmiqr1jha00i6ie8fb1scb3go_DoorProfiles_out_schema = {
+    "code": str,
+    "message": str,
+    "doorList": [{
+    "doorID": str,
+    "doorName": str,
+    "doorRelayStatus": str,
+    OptionalKey("doorSensor"): str,
+    OptionalKey("doorLoc"): {
+    "lon": str,
+    "lat": str,
+    OptionalKey("alt"): str,
+    OptionalKey("desc"): str,
+},
+    OptionalKey("bioDeviceList"): [{
+    OptionalKey("bioDeviceID"): str,
+    OptionalKey("bioDeviceName"): str,
+    "bioDeviceAuthTypeList": [str],
+}],
+    OptionalKey("otherDeviceList"): [{
+    OptionalKey("otherDeviceID"): str,
+    OptionalKey("otherDeviceName"): str,
+    "otherDeviceAuthTypeList": [str],
+}],
+}],
+}
+
+# RealtimeDoorStatus
+cmiqr1jha00i6ie8fb1scb3go_RealtimeDoorStatus_out_schema = {
+    "code": str,
+    "message": str,
+}
+
+# RealtimeDoorStatus WebHook IN Schema
+cmiqr1jha00i6ie8fb1scb3go_RealtimeDoorStatus_webhook_in_schema = {
+    "doorList": [{
+    "doorID": str,
+    "doorName": str,
+    OptionalKey("doorRelaySensor"): str,
+    OptionalKey("doorSensor"): str,
+}],
+}
+
+# DoorControl
+cmiqr1jha00i6ie8fb1scb3go_DoorControl_out_schema = {
+    "code": str,
+    "message": str,
+}
+
+# RealtimeDoorStatus
+cmiqr1jha00i6ie8fb1scb3go_RealtimeDoorStatus_out_schema = {
+    "code": str,
+    "message": str,
+}
+
+# RealtimeDoorStatus WebHook IN Schema
+cmiqr1jha00i6ie8fb1scb3go_RealtimeDoorStatus_webhook_in_schema = {
+    "doorList": [{
+    "doorID": str,
+    "doorName": str,
+    "doorRelaySensor": str,
+    "doorSensor": str,
+}],
+}
+
+# cmiqr1jha00i6ie8fb1scb3go 스키마 리스트
+cmiqr1jha00i6ie8fb1scb3go_outSchema = [
+    cmiqr1jha00i6ie8fb1scb3go_Authentication_out_schema,
+    cmiqr1jha00i6ie8fb1scb3go_Capabilities_out_schema,
+    cmiqr1jha00i6ie8fb1scb3go_DoorProfiles_out_schema,
+    cmiqr1jha00i6ie8fb1scb3go_RealtimeDoorStatus_out_schema,
+    cmiqr1jha00i6ie8fb1scb3go_DoorControl_out_schema,
+    cmiqr1jha00i6ie8fb1scb3go_RealtimeDoorStatus_out_schema,
+]
+
+# cmiqr1jha00i6ie8fb1scb3go WebHook 스키마 리스트
+cmiqr1jha00i6ie8fb1scb3go_webhook_inSchema = [
+    cmiqr1jha00i6ie8fb1scb3go_RealtimeDoorStatus_webhook_in_schema,
+    cmiqr1jha00i6ie8fb1scb3go_RealtimeDoorStatus_webhook_in_schema,
+]
+
+# Authentication
+cmiqr0kdw00i4ie8fr3firjtg_Authentication_out_schema = {
+    "code": str,
+    "message": str,
+    "": str,
+    "userName": str,
+    "userAff": str,
+    OptionalKey("accessToken"): str,
+}
+
+# Capabilities
+cmiqr0kdw00i4ie8fr3firjtg_Capabilities_out_schema = {
+    "code": str,
+    "message": str,
+    "streamingSupport": [{
+    "streamProtocolType": str,
+    OptionalKey("streamProtocolDesc"): str,
+}],
+    "transportSupport": [{
+    "transProtocolType": str,
+    OptionalKey("transProtocolDesc"): str,
+}],
 }
 
 # CameraProfiles
-cmii7v8pr006g8z1tvo55a50u_CameraProfiles_out_schema = {
+cmiqr0kdw00i4ie8fr3firjtg_CameraProfiles_out_schema = {
     "code": str,
     "message": str,
-    "camList": [str],
+    "camList": [{
+    "": {
+    "camID": str,
+    "camName": str,
+    OptionalKey("camLoc"): {
+    "lon": str,
+    "lat": str,
+    OptionalKey("alt"): str,
+    OptionalKey("desc"): str,
+},
+    "camConfig": {
+    "camType": str,
+},
+},
+}],
 }
 
-# StreamURLs
-cmii7v8pr006g8z1tvo55a50u_StreamURLs_out_schema = {
+# PtzStatus
+cmiqr0kdw00i4ie8fr3firjtg_PtzStatus_out_schema = {
     "code": str,
     "message": str,
-    "camList": [str],
+    OptionalKey("position"): {
+    OptionalKey("pan"): int,
+    OptionalKey("tilt"): int,
+    OptionalKey("zoom"): int,
+},
+    "moveStatus": {
+    OptionalKey("pan"): str,
+    OptionalKey("tilt"): str,
+    OptionalKey("zoom"): str,
+},
 }
 
-# RealtimeVideoEventInfos
-cmii7v8pr006g8z1tvo55a50u_RealtimeVideoEventInfos_out_schema = {
-    "": str,
+# PtzContinuousMove
+cmiqr0kdw00i4ie8fr3firjtg_PtzContinuousMove_out_schema = {
+    "code": str,
+    "message": str,
 }
 
-# RealtimeVideoEventInfos WebHook IN Schema
-cmii7v8pr006g8z1tvo55a50u_RealtimeVideoEventInfos_webhook_in_schema = {}
+# PtzStop
+cmiqr0kdw00i4ie8fr3firjtg_PtzStop_out_schema = {
+    "code": str,
+    "message": str,
+}
 
-# cmii7v8pr006g8z1tvo55a50u 스키마 리스트
-cmii7v8pr006g8z1tvo55a50u_outSchema = [
-    cmii7v8pr006g8z1tvo55a50u_Authentication_out_schema,
-    cmii7v8pr006g8z1tvo55a50u_Capabilities_out_schema,
-    cmii7v8pr006g8z1tvo55a50u_CameraProfiles_out_schema,
-    cmii7v8pr006g8z1tvo55a50u_StreamURLs_out_schema,
-    cmii7v8pr006g8z1tvo55a50u_RealtimeVideoEventInfos_out_schema,
-]
-
-# cmii7v8pr006g8z1tvo55a50u WebHook 스키마 리스트
-cmii7v8pr006g8z1tvo55a50u_webhook_inSchema = [
-    cmii7v8pr006g8z1tvo55a50u_RealtimeVideoEventInfos_webhook_in_schema,
+# cmiqr0kdw00i4ie8fr3firjtg 스키마 리스트
+cmiqr0kdw00i4ie8fr3firjtg_outSchema = [
+    cmiqr0kdw00i4ie8fr3firjtg_Authentication_out_schema,
+    cmiqr0kdw00i4ie8fr3firjtg_Capabilities_out_schema,
+    cmiqr0kdw00i4ie8fr3firjtg_CameraProfiles_out_schema,
+    cmiqr0kdw00i4ie8fr3firjtg_PtzStatus_out_schema,
+    cmiqr0kdw00i4ie8fr3firjtg_PtzContinuousMove_out_schema,
+    cmiqr0kdw00i4ie8fr3firjtg_PtzStop_out_schema,
 ]
 
--- spec/Validation_response.py
+++ spec/Validation_response.py
@@ -1,91 +1,118 @@
 # Authentication
-cmii7wfuf006i8z1tcds6q69g_Authentication_out_validation = {
-  "code": {
-    "enabled": True,
-    "validationType": "specified-value-match",
-    "allowedValues": [
-      "400"
-    ],
-    "score": 0
-  },
-  "message": {
-    "enabled": True,
-    "validationType": "specified-value-match",
-    "allowedValues": [
-      "잘못된 요청"
-    ],
-    "score": 0
-  }
-}
-
-# cmii7wfuf006i8z1tcds6q69g 검증 리스트
-cmii7wfuf006i8z1tcds6q69g_outValidation = [
-    cmii7wfuf006i8z1tcds6q69g_Authentication_out_validation,
-]
-
-# Authentication
-cmii7w683006h8z1t7usnin5g_Authentication_out_validation = {
-  "code": {
-    "enabled": True,
-    "validationType": "specified-value-match",
-    "allowedValues": [
-      "400"
-    ],
-    "score": 0
-  },
-  "message": {
-    "enabled": True,
-    "validationType": "specified-value-match",
-    "allowedValues": [
-      "잘못된 요청"
-    ],
-    "score": 0
-  }
-}
-
-# cmii7w683006h8z1t7usnin5g 검증 리스트
-cmii7w683006h8z1t7usnin5g_outValidation = [
-    cmii7w683006h8z1t7usnin5g_Authentication_out_validation,
-]
-
-# Authentication
-cmii7v8pr006g8z1tvo55a50u_Authentication_out_validation = {
-  "code": {
-    "enabled": True,
-    "validationType": "specified-value-match",
-    "allowedValues": [
-      "400"
-    ],
-    "score": 0
-  },
-  "message": {
-    "enabled": True,
-    "validationType": "specified-value-match",
-    "allowedValues": [
-      "잘못된 요청"
-    ],
-    "score": 0
-  }
-}
+cmiqr2b9j00i9ie8frw439h8i_Authentication_out_validation = {}
 
 # Capabilities
-cmii7v8pr006g8z1tvo55a50u_Capabilities_out_validation = {}
+cmiqr2b9j00i9ie8frw439h8i_Capabilities_out_validation = {}
+
+# SensorDeviceProfiles
+cmiqr2b9j00i9ie8frw439h8i_SensorDeviceProfiles_out_validation = {}
+
+# SensorDeviceControl
+cmiqr2b9j00i9ie8frw439h8i_SensorDeviceControl_out_validation = {
+  "sensorDeviceID": {
+    "enabled": True,
+    "validationType": "request-field-match",
+    "referenceFieldId": "cmisfqqxc07ub5vy7ao1d67bu",
+    "referenceField": "sensorDeviceID",
+    "referenceEndpoint": "/SensorDeviceControl",
+    "score": 0
+  },
+  "sensorDeviceStatus": {
+    "enabled": True,
+    "validationType": "valid-value-match",
+    "validValueMatchType": "validation-field",
+    "validValueFieldName": "sensorControl",
+    "validValueOperator": "equalsAny",
+    "allowedValues": [
+      "AlarmOn",
+      "AlarmOff"
+    ],
+    "score": 0
+  }
+}
+
+# SensorDeviceControl2
+cmiqr2b9j00i9ie8frw439h8i_SensorDeviceControl2_out_validation = {
+  "sensorDeviceID": {
+    "enabled": True,
+    "validationType": "request-field-match",
+    "referenceFieldId": "cmisg8fmw08c55vy7eby9fson",
+    "referenceField": "sensorDeviceID",
+    "referenceEndpoint": "/SensorDeviceControl",
+    "score": 0
+  },
+  "sensorDeviceStatus": {
+    "enabled": True,
+    "validationType": "request-field-match",
+    "referenceFieldId": "cmisg8hg208ca5vy7ijzfeelo",
+    "referenceField": "commandType",
+    "referenceEndpoint": "/SensorDeviceControl",
+    "score": 0
+  }
+}
+
+# cmiqr2b9j00i9ie8frw439h8i 검증 리스트
+cmiqr2b9j00i9ie8frw439h8i_outValidation = [
+    cmiqr2b9j00i9ie8frw439h8i_Authentication_out_validation,
+    cmiqr2b9j00i9ie8frw439h8i_Capabilities_out_validation,
+    cmiqr2b9j00i9ie8frw439h8i_SensorDeviceProfiles_out_validation,
+    cmiqr2b9j00i9ie8frw439h8i_SensorDeviceControl_out_validation,
+    cmiqr2b9j00i9ie8frw439h8i_SensorDeviceControl2_out_validation,
+]
+
+# Authentication
+cmiqr1jha00i6ie8fb1scb3go_Authentication_out_validation = {}
+
+# Capabilities
+cmiqr1jha00i6ie8fb1scb3go_Capabilities_out_validation = {}
+
+# DoorProfiles
+cmiqr1jha00i6ie8fb1scb3go_DoorProfiles_out_validation = {}
+
+# RealtimeDoorStatus
+cmiqr1jha00i6ie8fb1scb3go_RealtimeDoorStatus_out_validation = {}
+
+# DoorControl
+cmiqr1jha00i6ie8fb1scb3go_DoorControl_out_validation = {}
+
+# RealtimeDoorStatus
+cmiqr1jha00i6ie8fb1scb3go_RealtimeDoorStatus_out_validation = {}
+
+# cmiqr1jha00i6ie8fb1scb3go 검증 리스트
+cmiqr1jha00i6ie8fb1scb3go_outValidation = [
+    cmiqr1jha00i6ie8fb1scb3go_Authentication_out_validation,
+    cmiqr1jha00i6ie8fb1scb3go_Capabilities_out_validation,
+    cmiqr1jha00i6ie8fb1scb3go_DoorProfiles_out_validation,
+    cmiqr1jha00i6ie8fb1scb3go_RealtimeDoorStatus_out_validation,
+    cmiqr1jha00i6ie8fb1scb3go_DoorControl_out_validation,
+    cmiqr1jha00i6ie8fb1scb3go_RealtimeDoorStatus_out_validation,
+]
+
+# Authentication
+cmiqr0kdw00i4ie8fr3firjtg_Authentication_out_validation = {}
+
+# Capabilities
+cmiqr0kdw00i4ie8fr3firjtg_Capabilities_out_validation = {}
 
 # CameraProfiles
-cmii7v8pr006g8z1tvo55a50u_CameraProfiles_out_validation = {}
+cmiqr0kdw00i4ie8fr3firjtg_CameraProfiles_out_validation = {}
 
-# StreamURLs
-cmii7v8pr006g8z1tvo55a50u_StreamURLs_out_validation = {}
+# PtzStatus
+cmiqr0kdw00i4ie8fr3firjtg_PtzStatus_out_validation = {}
 
-# RealtimeVideoEventInfos
-cmii7v8pr006g8z1tvo55a50u_RealtimeVideoEventInfos_out_validation = {}
+# PtzContinuousMove
+cmiqr0kdw00i4ie8fr3firjtg_PtzContinuousMove_out_validation = {}
 
-# cmii7v8pr006g8z1tvo55a50u 검증 리스트
-cmii7v8pr006g8z1tvo55a50u_outValidation = [
-    cmii7v8pr006g8z1tvo55a50u_Authentication_out_validation,
-    cmii7v8pr006g8z1tvo55a50u_Capabilities_out_validation,
-    cmii7v8pr006g8z1tvo55a50u_CameraProfiles_out_validation,
-    cmii7v8pr006g8z1tvo55a50u_StreamURLs_out_validation,
-    cmii7v8pr006g8z1tvo55a50u_RealtimeVideoEventInfos_out_validation,
+# PtzStop
+cmiqr0kdw00i4ie8fr3firjtg_PtzStop_out_validation = {}
+
+# cmiqr0kdw00i4ie8fr3firjtg 검증 리스트
+cmiqr0kdw00i4ie8fr3firjtg_outValidation = [
+    cmiqr0kdw00i4ie8fr3firjtg_Authentication_out_validation,
+    cmiqr0kdw00i4ie8fr3firjtg_Capabilities_out_validation,
+    cmiqr0kdw00i4ie8fr3firjtg_CameraProfiles_out_validation,
+    cmiqr0kdw00i4ie8fr3firjtg_PtzStatus_out_validation,
+    cmiqr0kdw00i4ie8fr3firjtg_PtzContinuousMove_out_validation,
+    cmiqr0kdw00i4ie8fr3firjtg_PtzStop_out_validation,
 ]
 
