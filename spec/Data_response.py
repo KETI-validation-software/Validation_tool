919f172eb0cc6173a3460caf2a60faf613153da3
keseo <keseo9513@gmail.com>
2025-12-22 15:41:55 +0900

    웹훅 constrains 적용 등 보완

--- spec/Data_response.py
+++ spec/Data_response.py
@@ -218,8 +218,20 @@ cmiqr1acx00i5ie8fi022t1hp_DoorProfiles_out_data = {
 
 # RealtimeDoorStatus
 cmiqr1acx00i5ie8fi022t1hp_RealtimeDoorStatus_out_data = {
-    "code": "",
-    "message": ""
+    "code": "성공",
+    "message": "200"
+}
+
+# RealtimeDoorStatus WebHook IN Data
+cmiqr1acx00i5ie8fi022t1hp_RealtimeDoorStatus_webhook_in_data = {
+    "doorList": [
+        {
+        "doorID": "",
+        "doorName": "",
+        "doorRelaySensor": "일반",
+        "doorSensor": ""
+    }
+]
 }
 
 # DoorControl
@@ -261,7 +273,7 @@ cmiqr1acx00i5ie8fi022t1hp_webhook_inData = [
     None,
     None,
     None,
-    None,
+    cmiqr1acx00i5ie8fi022t1hp_RealtimeDoorStatus_webhook_in_data,
     None,
     cmiqr1acx00i5ie8fi022t1hp_RealtimeDoorStatus2_webhook_in_data,
 ]
