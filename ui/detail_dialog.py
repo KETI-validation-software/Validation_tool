848b077c2c049149de18d3d6cca732779731a065
dpwlsssss <yaezinny95@naver.com>
2026-01-08 11:51:53 +0900

    Feat: 스크롤바 적용

--- ui/detail_dialog.py
+++ ui/detail_dialog.py
@@ -76,8 +76,36 @@ class CombinedDetailDialog(QDialog):
 
         # 공통 스타일
         title_style = "font-family: 'Noto Sans KR'; font-size: 18px; font-weight: 600;"
-        box_style = "border: 1px solid #CECECE; border-radius: 4px; background-color: #FFFFFF; font-family: 'Noto Sans KR'; font-size: 19px; font-weight: 400; padding: 12px;"
-
+        box_style = """
+              QTextBrowser {
+                  border: 1px solid #CECECE;
+                  border-radius: 4px;
+                  background-color: #FFFFFF;
+                  font-family: 'Noto Sans KR';
+                  font-size: 19px;
+                  font-weight: 400;
+                  padding: 12px;
+              }
+              QScrollBar:vertical {
+                  border: none;
+                  background: #DFDFDF;
+                  width: 14px;
+                  margin: 0px;
+                  border-radius: 4px;
+              }
+              QScrollBar::handle:vertical {
+                  background: #A3A9AD;
+                  min-height: 20px;
+                  border-radius: 4px;
+                  margin: 0px 3px;
+              }
+              QScrollBar::handle:vertical:hover {
+                  background: #8A9094;
+              }
+              QScrollBar::add-line:vertical, QScrollBar::sub-line:vertical {
+                  height: 0px;
+              }
+          """
         # 1열: 메시지 데이터 - 반응형: 동일 비율 확장
         data_column = QWidget()
         data_column.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
